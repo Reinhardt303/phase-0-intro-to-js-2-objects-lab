4559ac31047fb2e70265a36c69b55ee2e8acd603
Thomas Rains <thomasrains01@gmail.com>
2024-09-26 15:08:59 -0700

    uploads objects lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+const employee = {
+    name: "defaultName",
+    streetAddress: "defaultAddy",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value,) {
+    return{
+        ...obj,
+        [key]: value,
+    }
+}
+
+const newEmployee = updateEmployeeWithKeyAndValue(
+    employee, defaultName, "updatedName"
+);
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value,) {
+    obj[key] = value; 
+
+  return obj;
+}
+
+const destructedEmployee = destructivelyUpdateEmployeeWithKeyAndValue(
+    employee, defaultName, "updatedName"
+);
+
+function deleteFromEmployeeByKey() {
+    const removedKeyEmployee = {...employee}
+    delete removedKeyEmployee.name
+    return removedKeyEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name
+    return employee;
+}
+
